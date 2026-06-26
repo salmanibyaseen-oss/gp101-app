ae0004179816b50c324b9b925d788ae115f40a93
salmanibyaseen-oss <salman.ib.yaseen@gmail.com>
2026-06-26 18:25:11 +0300

    Delete src directory

--- src/app/api/slugs/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { content } from "@/lib/content";
-
-export async function GET() {
-  const slugs = content.sections.flatMap((s) =>
-    s.subsections.flatMap((sub) => sub.topics.map((t) => t.slug))
-  );
-  return NextResponse.json({ slugs });
-}
